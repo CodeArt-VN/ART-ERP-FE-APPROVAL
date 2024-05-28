df3f70278af63018cf7caf32611fb4184250ee9a
Hùng Vũ <hungvq@live.com>
2024-05-28 09:09:05 +0700

    clean

--- routing.module.ts
+++ routing.module.ts
@@ -3,13 +3,13 @@ import { AuthGuard } from 'src/app/guards/app.guard';
 
 export const APPROVALRoutes: Routes = [
     
-    { path: 'request', loadChildren: () => import('./request/request.module').then(m => m.RequestPageModule), canActivate: [AuthGuard] },
-    { path: 'request/:id', loadChildren: () => import('./request-detail/request-detail.module').then(m => m.RequestDetailPageModule), canActivate: [AuthGuard] },
+    // { path: 'request', loadChildren: () => import('./request/request.module').then(m => m.RequestPageModule), canActivate: [AuthGuard] },
+    // { path: 'request/:id', loadChildren: () => import('./request-detail/request-detail.module').then(m => m.RequestDetailPageModule), canActivate: [AuthGuard] },
 
-    { path: 'approval-rule', loadChildren: () => import('./approval-rule/approval-rule.module').then(m => m.ApprovalRulePageModule), canActivate: [AuthGuard] },
-    { path: 'approval-rule/:id', loadChildren: () => import('./approval-rule-detail/approval-rule-detail.module').then(m => m.ApprovalRuleDetailPageModule), canActivate: [AuthGuard] },
+    // { path: 'approval-rule', loadChildren: () => import('./approval-rule/approval-rule.module').then(m => m.ApprovalRulePageModule), canActivate: [AuthGuard] },
+    // { path: 'approval-rule/:id', loadChildren: () => import('./approval-rule-detail/approval-rule-detail.module').then(m => m.ApprovalRuleDetailPageModule), canActivate: [AuthGuard] },
   
-    { path: 'approval-template', loadChildren: () => import('./approval-template/approval-template.module').then(m => m.ApprovalTemplatePageModule), canActivate: [AuthGuard] },
-    { path: 'approval-template/:id', loadChildren: () => import('./approval-template-detail/approval-template-detail.module').then(m => m.ApprovalTemplateDetailPageModule), canActivate: [AuthGuard] },
+    // { path: 'approval-template', loadChildren: () => import('./approval-template/approval-template.module').then(m => m.ApprovalTemplatePageModule), canActivate: [AuthGuard] },
+    // { path: 'approval-template/:id', loadChildren: () => import('./approval-template-detail/approval-template-detail.module').then(m => m.ApprovalTemplateDetailPageModule), canActivate: [AuthGuard] },
   
 ];
