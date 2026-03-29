0a573205389d9f30b0b50474eeb8fdf36da2445c
Hùng Vũ <hungvq@live.com>
2026-03-29 22:00:38 +0700

    Replace Term with Keyword

--- approval-rule-detail/approval-rule-detail.page.ts
+++ approval-rule-detail/approval-rule-detail.page.ts
@@ -177,7 +177,7 @@ export class ApprovalRuleDetailPage extends PageBase {
 			// 				distinctUntilChanged(),
 			// 				tap(() => (this.loading = true)),
 			// 				switchMap((term) =>
-			// 					this.searchProvider.search({ Take: 20, Skip: 0, Term: term }).pipe(
+			// 					this.searchProvider.search({ Take: 20, Skip: 0, Keyword: term }).pipe(
 			// 						catchError(() => of([])), // empty list on error
 			// 						tap(() => (this.loading = false))
 			// 					)
